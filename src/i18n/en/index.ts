a5ca776bd81680e72c3f0175ec2500c405678a61
BrilliantDeviation7 <kevin2006.zhu@gmail.com>
2023-01-07 20:57:06 -0500

    Update index.ts

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -3,7 +3,7 @@ import type { BaseTranslation } from '../i18n-types';
 const en: BaseTranslation = {
   Tags: 'Tags',
   FilterTags: 'Filter Tags...',
-  TableOfContent: 'Table of Content',
+  TableOfContent: 'Table of Contents',
   LoadingPosts: 'Loading Posts...',
   LoadingPost: 'Loading Post...',
   NoPostFound: 'No Post Found.',
